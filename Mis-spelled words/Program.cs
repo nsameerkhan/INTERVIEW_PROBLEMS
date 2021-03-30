6257ffdba04618ad5514122ad51b47881badca90
nsameerkhan <nsameerkhan1@gmail.com>
2021-03-31 00:15:56 +0530

    changes

--- Mis-spelled words/Program.cs
+++ Mis-spelled words/Program.cs
@@ -10,22 +10,26 @@ namespace Mis_spelled_words
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("CONTAINS WORDS");
+            Console.WriteLine("speed" +"\t"+ "time" +"\t"+ "coffee"+"\t"+ "saturday"+"\t"+ "man");
             string value =Console.ReadLine();
             misspelled(value);
         }
         public static void misspelled(string a)
         {
             int count = 0;
+           
             List<char> l1 = new List<char> {'s','p','e','e','d'};
             List<char> l2 = new List<char> { 't', 'i', 'm', 'e' };
             List<char> l3 = new List<char> { 'c', 'o', 'f', 'f', 'e', 'e' };
             List<char> l4 = new List<char> { 's','a','t','u','r','d','a','y' };
+            List<char> l5 = new List<char> { 'm', 'a', 'n' };
             for (int i = 0; i < a.Length; i++)
             {
                 
-                if (a[0] == l1.First()||a[0]==l2.First()||a[0]==l3.First()||a[0]==l4.First())
+                if (a[0] == l1.First()||a[0]==l2.First()||a[0]==l3.First()||a[0]==l4.First()||a[0]==l5.First())
                 {
-                    if (l1.Contains(a[i])||l2.Contains(a[i])||l3.Contains(a[i])||l4.Contains(a[i]))
+                    if (l1.Contains(a[i])||l2.Contains(a[i])||l3.Contains(a[i])||l4.Contains(a[i])||l5.Contains(a[i]))
                     {
                         count++;
                     }
@@ -66,6 +70,15 @@ namespace Mis_spelled_words
                 else if(l4.Count==count)
                 {
                     foreach (var ans in l4)
+                    {
+                        Console.Write(ans);
+                    }
+                    Console.WriteLine();
+
+                }
+                else if (l5.Count == count)
+                {
+                    foreach (var ans in l5)
                     {
                         Console.Write(ans);
                     }
